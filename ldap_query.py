5ab1b14c38d6a4f3169f431d56e9743233d9df02
vsrz <ritual@vsritual.com>
2013-03-15 12:22:57 -0700

    Update script

--- ldap_query.py
+++ ldap_query.py
@@ -1,51 +1,53 @@
-import ldap
+#!/usr/bin/python
+#
+# Ldap query on an DN and write to file
+#
+#
+#
+
 import sys
 import time
+import ldap
+
+f = open('out.txt','w')
+
+LDAP_PROTO = ''
+LDAP_SERVER = ''
+LDAP_BIND_DN = ''
+LDAP_BIND_PW = ''
+
+# Setup Search params
+LDAP_BASEDN = 'ou=People,dc=example,dc=com'
+LDAP_SEARCH_SCOPE = ldap.SCOPE_SUBTREE
+
+# Retrieve attributes
+RETRIEVE_ATTRIBUTES = ['uid','pwdAccountLockedTime']
+SEARCH_FILTER = 'pwdAccountLockedTime=*'
+
 
 # Setup LDAP Conn
 try:
 
-	l = ldap.open('')
+        l = ldap.initialize(LDAP_PROTO + '://' + LDAP_SERVER)
 
-	l.protocol_version = ldap.VERSION3
+        l.protocol_version = ldap.VERSION3
 
-	username = ''
-	password = ''
+        username = LDAP_BIND_DN
+        password = LDAP_BIND_PW
 
-	l.simple_bind(username,password)
+        l.simple_bind_s(username,password)
 
-except ldap.LDAPerror, e:
-	print e
-
-# Setup Search params
-baseDN = 'ou=students,ou=academic,dc=csusm,dc=edu'
-searchScope = ldap.SCOPE_SUBTREE
-
-# Retrieve only whats needed
-#retrieveAttributes = ['sAMAccountName','employeeID','mail']
-retrieveAttributes = ['sAMAccountName','displayName','description','employeeID','whenCreated','mail']
-#searchFilter = 'sAMAccountName=ville017'
-searchFilter = 'whenCreated=20120622223453.0Z';
-
-try: 
-	ldap_result_id = l.search(baseDN, searchScope, searchFilter, retrieveAttributes)
-	result_set = []
-	while 1:
-		result_type, result_data = l.result(ldap_result_id, 0)
-		if(result_data == []):
-			break
-		else:
-			if result_type == ldap.RES_SEARCH_ENTRY:
-				result_set.append(result_data)
-	#print result_set
-
-	# result set
-	x = 0
-	for each in result_set:
-		# re
-		for e in each:
-			x += 1
-			print str(e[1]['sAMAccountName'][0]) + ',' + str(e[1]['displayName'][0]) + ',' +str(e[1]['description'][0]) + ',' +str(e[1]['employeeID'][0]) + ',' + str(e[1]['whenCreated'][0][0:8]) + ',' + str(e[1]['mail'][0])
 except ldap.LDAPError, e:
-	#print e
-	exit
+        print e
+
+
+try:
+        ldap_result = l.search_s(LDAP_BASEDN, ldap.SCOPE_SUBTREE, SEARCH_FILTER, RETRIEVE_ATTRIBUTES)
+        for dn,entry in ldap_result:
+                acct = str(dn)
+                f.write(acct + '\n')
+
+except ldap.LDAPError, e:
+        print 'Error %s retrieving' % str(e)
+
+
